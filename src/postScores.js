243d6cab7e37fe3d780dc71f765ce4321c62a78f
PrinceBadru <100031675+PrinceBadru@users.noreply.github.com>
2023-04-22 12:48:31 +0300

    Post Score changes

--- src/postScores.js
+++ src/postScores.js
@@ -1,21 +1,17 @@
 const nameInput = document.getElementById('name');
 const scoreInput = document.getElementById('score');
 
-const postScores = async (url) => {
-  const result = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify({
-      user: nameInput.value,
-      score: scoreInput.value,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
+const result = await fetch(url, {
+  method: 'POST',
+  body: JSON.stringify({
+    user: nameInput.value,
+    score: scoreInput.value,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+});
 
-  nameInput.value = '';
-  scoreInput.value = '';
-  return result.json();
-};
-
-export default postScores;
+nameInput.value = '';
+scoreInput.value = '';
+return result.json();
